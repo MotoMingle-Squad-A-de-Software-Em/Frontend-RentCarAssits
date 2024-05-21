90b9201cecc14ef3e8f27de051fa96322b21147b
Leonardo Aquino <u20201b949@upc.edu.pe>
2024-05-20 22:10:18 -0500

    Add RentCarAssits project in Angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+
+import { AppComponent } from "./app.component";
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    CommonModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+
+}
