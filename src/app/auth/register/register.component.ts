dc094f4bdbd2b68a55f0463bb38e60868999d65c
Leonardo Aquino <u20201b949@upc.edu.pe>
2024-05-27 21:47:50 -0500

    Add Auth

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormControl} from "@angular/forms";
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  fileControl = new FormControl();
 
+  constructor() {
+  }
+
+  onFileChange(event: Event) {
+    const element = event.currentTarget as HTMLInputElement;
+    let fileList: FileList | null = element.files;
+    if (fileList) {
+      console.log("Nombre del archivo:", fileList[0].name);
+      this.fileControl.setValue(fileList[0]);
+    }
+  }
 }
