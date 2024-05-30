32f4f5a8f5b2ee2b0f6cd697849dc257831b77d4
Leonardo Cesías Díaz <u202010773@upc.edu.pe>
2024-05-29 22:50:51 -0500

    Fixed car-card component

---
+++ src/app/cars/car-card/car-card.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-car-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './car-card.component.html',
+  styleUrl: './car-card.component.css'
+})
+export class CarCardComponent {
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
