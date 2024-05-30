9154c92b7200433b6a068922e250d229c6d3c9f5
Leonardo Cesías Díaz <u202010773@upc.edu.pe>
2024-05-29 23:07:14 -0500

    Updated car-card for api information

--- src/app/cars/car-card/car-card.component.ts
+++ src/app/cars/car-card/car-card.component.ts
@@ -1,16 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CarDataService} from "../car-data/car-data.component";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-car-card',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   templateUrl: './car-card.component.html',
   styleUrl: './car-card.component.css'
 })
-export class CarCardComponent {
-  constructor() { }
+export class CarCardComponent implements OnInit {
+
+  carData: any[] = [];
+
+  constructor(private carDataService: CarDataService) { }
 
   ngOnInit(): void {
+    this.carDataService.getCarData().subscribe(data => {
+      this.carData = data.Results;
+    });
   }
-
 }
