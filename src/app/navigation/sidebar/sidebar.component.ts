54dc1b00129c5d6b75c5d7f67cbdb63ba76515ed
Leonardo Cesías Díaz <u202010773@upc.edu.pe>
2024-05-31 14:13:34 -0500

    Added routing for search and cars on sidebar

--- src/app/navigation/sidebar/sidebar.component.ts
+++ src/app/navigation/sidebar/sidebar.component.ts
@@ -1,20 +1,28 @@
 import {Component, OnInit} from '@angular/core';
 import {MatIcon} from "@angular/material/icon";
+import {Router, RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
   imports: [
-    MatIcon
+    MatIcon,
+    RouterLink
   ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent implements OnInit {
+  isMyCarsActive: boolean = false;
+  isSearchActive: boolean = false;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    this.router.events.subscribe((val) => {
+      this.isMyCarsActive = this.router.url === '/mycars';
+      this.isSearchActive = this.router.url === '/home';
+    });
   }
 
 }
