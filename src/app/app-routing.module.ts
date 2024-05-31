b0ded0e1b30b087568215690a0c01cb0be655e3b
Leonardo Cesías Díaz <u202010773@upc.edu.pe>
2024-05-31 12:40:02 -0500

    Updated routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import {LoginComponent} from "./auth/login/login.component";
 import {RegisterComponent} from "./auth/register/register.component";
 import {CarCardComponent} from "./cars/car-card/car-card.component";
 import {HomeViewComponent} from "./views/home-view/home-view.component";
+import {MycarsViewComponent} from "./views/mycars-view/mycars-view.component";
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
@@ -11,6 +12,7 @@ const routes: Routes = [
   { path: 'register', component: RegisterComponent, pathMatch: 'full' },
   { path: 'car-card', component: CarCardComponent, pathMatch: 'full' },
   { path: 'home', component: HomeViewComponent, pathMatch: 'full' },
+  { path: 'mycars', component: MycarsViewComponent, pathMatch: 'full' },
 ]
 
 @NgModule({
