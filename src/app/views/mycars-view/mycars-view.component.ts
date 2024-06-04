6d6a87679fd2c7b12590bd8db010adec97a23ed7
Leonardo Cesías Díaz <u202010773@upc.edu.pe>
2024-06-04 17:57:02 -0500

    Improved navbar in mycars view

--- src/app/views/mycars-view/mycars-view.component.ts
+++ src/app/views/mycars-view/mycars-view.component.ts
@@ -5,17 +5,19 @@ import {CarAddComponent} from "../../cars/car-add/car-add.component";
 import {MatDialog} from "@angular/material/dialog";
 import {MatButton} from "@angular/material/button";
 import {CarOwnerCardComponent} from "../../cars/car-owner-card/car-owner-card.component";
+import {NavbarComponent} from "../../navigation/navbar/navbar.component";
 
 @Component({
   selector: 'app-mycars-view',
   standalone: true,
-  imports: [
-    CarCardComponent,
-    SidebarComponent,
-    CarAddComponent,
-    MatButton,
-    CarOwnerCardComponent,
-  ],
+    imports: [
+        CarCardComponent,
+        SidebarComponent,
+        CarAddComponent,
+        MatButton,
+        CarOwnerCardComponent,
+        NavbarComponent,
+    ],
   templateUrl: './mycars-view.component.html',
   styleUrl: './mycars-view.component.css'
 })
