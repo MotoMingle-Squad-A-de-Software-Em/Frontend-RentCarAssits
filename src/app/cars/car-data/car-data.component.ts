7cf8c8cacb39251d8035d4d95b8de6babaa80110
Leonardo Cesías Díaz <lpcd_04@hotmail.com>
2024-06-24 16:22:58 -0500

    Updated car data

--- src/app/cars/car-data/car-data.component.ts
+++ src/app/cars/car-data/car-data.component.ts
@@ -1,17 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarDataService {
 
-  private apiUrl = 'https://vpic.nhtsa.dot.gov/api/vehicles/GetWMIsForManufacturer/hon?format=json';
+  private apiUrl = 'https://zonal-charisma-production.up.railway.app/api/v1/vehicles';
 
   constructor(private http: HttpClient) { }
 
-  getCarData(): Observable<any> {
-    return this.http.get<any>(this.apiUrl);
+  getCarData(): Observable<any[]> {
+    return this.http.get<any>(this.apiUrl).pipe(
+      map(response => response.content)
+    );
+  }
+
+  addCar(carData: any): Observable<any> {
+    return this.http.post<any>(this.apiUrl, carData);
   }
 }
