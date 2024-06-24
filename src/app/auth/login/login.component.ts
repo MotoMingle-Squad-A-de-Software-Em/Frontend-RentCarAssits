087a97a4c5ad19505a4a7c571736b827be9c68a6
Leonardo Cesías Díaz <lpcd_04@hotmail.com>
2024-06-24 16:57:17 -0500

    Updated login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,18 +1,66 @@
 import {Component, Optional} from '@angular/core';
 import {MatDialogRef} from "@angular/material/dialog";
+import {Router} from "@angular/router";
+import {HttpClient} from "@angular/common/http";
+
+interface User {
+  id: number;
+  username: string;
+  password: string;
+  email: string;
+  names: string;
+  lastnames: string;
+  dni: string;
+  profilePicture: string;
+}
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  users: User[] = []; // Arreglo para almacenar los usuarios obtenidos del servicio
+  errorMessage: string = ''; // Variable para manejar mensajes de error
+
+  constructor(
+    @Optional() public dialogRef: MatDialogRef<LoginComponent>,
+    private router: Router,
+    private http: HttpClient
+  ) {
+    this.fetchUsers(); // Llamada al método para obtener usuarios al inicializar el componente
+  }
+
   closeDialog(): void {
     if (this.dialogRef) {
       this.dialogRef.close(); // Método para cerrar el diálogo
     }
   }
 
-  constructor(@Optional() public dialogRef: MatDialogRef<LoginComponent>) {
+  onSubmit(username: string, password: string): void {
+    // Validación simple de credenciales
+    const user = this.users.find(u => u.username === username && u.password === password);
+
+    if (user) {
+      // Usuario válido, redirige al home page
+      this.router.navigate(['/home']);
+      this.closeDialog();
+    } else {
+      // Usuario no encontrado o credenciales inválidas
+      this.errorMessage = 'Invalid username or password. Please try again.';
+    }
+  }
+
+  private fetchUsers(): void {
+    this.http.get<any>('https://zonal-charisma-production.up.railway.app/api/v1/users')
+      .subscribe(
+        response => {
+          this.users = response.content;
+        },
+        error => {
+          console.error('Error fetching users:', error);
+          this.errorMessage = 'Failed to fetch users. Please try again later.';
+        }
+      );
   }
 }
